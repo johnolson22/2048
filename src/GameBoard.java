938aa0c0d1a0a6ccb4dbf9584b73d712d6668351
Drew Csontos <35618647+drewcsontos@users.noreply.github.com>
2021-09-29 10:13:12 -0400

    refactored row and col literals to variables, added collapse methods

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 
 public class GameBoard {
 	
-	private static int[][] board = new int[4][4];
+	private int rows = 4;
+	private int cols = 4;
+	private static int[][] board = new int[rows][cols];
 	
 	public GameBoard() {
 		insertBlock();
@@ -23,7 +25,53 @@ public class GameBoard {
 		System.out.println(spot[1]);
 		board[spot[0]][spot[1]] = 1;
 	}
-	
+	private static void combineRight() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < rows; i++) {
+			for (int j = cols - 1; j > 0; j--) {
+				if (board[i][j - 1] == board[i][j]) {
+					board[i][j] *= 2;
+					board[i][j - 1] = 0;
+				}
+			}
+		}
+	}
+
+	private static void combineDown() {
+		// TODO Auto-generated method stub
+		for (int i = rows - 1; i > 0; i--) {
+			for (int j = 0; j < cols; j++) {
+				if (board[i - 1][j] == board[i][j]) {
+					board[i][j] *= 2;
+					board[i - 1][j] = 0;
+				}
+			}
+		}
+	}
+
+	private static void combineLeft() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols - 1; j++) {
+				if (board[i][j + 1] == board[i][j]) {
+					board[i][j] *= 2;
+					board[i][j + 1] = 0;
+				}
+			}
+		}
+	}
+
+	private static void combineUp() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < rows - 1; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (board[i + 1][j] == board[i][j]) {
+					board[i][j] *= 2;
+					board[i + 1][j] = 0;
+				}
+			}
+		}
+	}
 	public static void main(String[] args) {
 		
 		insertBlock();
